43eb83c44b475f1dc988a46b2052abad40a4ef1e
Makhmudjonov <86299087+Makhmudjonov@users.noreply.github.com>
2021-07-09 17:23:27 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = '1826837902:AAHx7J-20PrO01OybGAboSlmmyMLGI-3KJg' # Токен бота
+loginbot = '@hufbdjvbot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+QIWI_TOKEN = 'xxxxxxxx' # токен киви
+QIWI_NUMBER = '77777777777' # Номер киви без +
+
+admin_id = '1017975944' # Ваш айди
+
+price = 190 # Цена за доступ
